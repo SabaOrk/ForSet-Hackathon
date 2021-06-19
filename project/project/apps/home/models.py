4f0425d6a5319f8e2af4ec3953fa56184b528c8c
Saba Orkoshneli <saba.orkoshneli16@gmail.com>
2021-06-19 15:35:17 +0400

    Update models.py

--- project/project/apps/home/models.py
+++ project/project/apps/home/models.py
@@ -7,9 +7,14 @@ class Category(models.Model):
 	name = models.CharField(max_length=100)
 	description = models.CharField(max_length=500)
 	
+	def __str__(self):
+        return self.name
 
 class Topic(models.Model):
 	title = models.CharField(max_length=255)
 	main_category = models.OneToOneField(Category, default=None, on_delete=models.CASCADE, related_name='main_category')
 	sub_category = models.OneToOneField(Category, default=None, on_delete=models.CASCADE, related_name='sub_category')
 	count = models.IntegerField(default=0)
+
+	def __str__(self):
+        return self.title
