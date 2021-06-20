7e9a175cac18c76ad73f6841035388dbd6a9ce82
Saba Orkoshneli <saba.orkoshneli16@gmail.com>
2021-06-20 16:42:14 +0400

    3

--- project/project/apps/home/models.py
+++ project/project/apps/home/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Category(models.Model):
 	name = models.CharField(max_length=100)
 	description = models.CharField(max_length=500)
+	bg = models.CharField(max_length=30)
 	
 	def __str__(self):
 		return self.name
