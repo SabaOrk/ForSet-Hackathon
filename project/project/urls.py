7f8b08ef522870d061b71079962810a1c8890c5b
Saba Orkoshneli <saba.orkoshneli16@gmail.com>
2021-06-20 14:19:03 +0400

    Update urls.py

--- project/project/urls.py
+++ project/project/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from project.apps.home import views as home_views
+from project.apps.subcategory import views as subcategory_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
